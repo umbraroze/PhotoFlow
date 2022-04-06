eac55876420ce4e0cfbc1ffe02a4e4a9faf8c283
Rose Midford <wwwwolf@iki.fi>
2022-04-06 18:06:23 +0300

    hacky thing to do .nef too

--- fix_memorycard_recovery.ps1
+++ fix_memorycard_recovery.ps1
@@ -1,11 +1,23 @@
-# Postprocessing stuff from Recuva SD card recovery. Results in files
-# like "[000001].tif", with DNG files identified as TIFFs. Rename all
-# files to img000001.dng or .jpg as appropriate. Fix file timestamps
-# using exiftool.
+<#
+    Postprocessing stuff from Recuva SD card recovery. Results in files
+    like "[000001].tif", with DNG files identified as TIFFs. Rename all
+    files to recov_img000001.dng or .nef or .jpg as appropriate. Fix file
+    timestamps using exiftool.
+#>
 foreach ($item in (Get-Item *.tif)) {
     $oldname = ($item.Name)
     if($oldname -match '[\[\]]') {
-        $newname = ("img" + $item.Basename + ".dng") -Replace '[\[\]]',''
+        $newname = ("recov_img" + $item.Basename + ".dng") -Replace '[\[\]]',''
+        Move-Item -LiteralPath $oldname -Destination $newname
+        Write-Output "${oldname} -> ${newname}"
+    } else {
+        Write-Output "${oldname} OK"
+    }
+}
+foreach ($item in (Get-Item *.nef)) { # COPYPASTEHACK
+    $oldname = ($item.Name)
+    if($oldname -match '[\[\]]') {
+        $newname = ("recov_img" + $item.Name) -Replace '[\[\]]',''
         Move-Item -LiteralPath $oldname -Destination $newname
         Write-Output "${oldname} -> ${newname}"
     } else {
@@ -15,7 +27,7 @@ foreach ($item in (Get-Item *.tif)) {
 foreach ($item in (Get-Item *.jpg)) {
     $oldname = ($item.Name)
     if($oldname -match '[\[\]]') {
-        $newname = ("img" + $item.Name) -Replace '[\[\]]',''
+        $newname = ("recov_img" + $item.Name) -Replace '[\[\]]',''
         Move-Item -LiteralPath $oldname -Destination $newname
         Write-Output "${oldname} -> ${newname}"
     } else {
@@ -23,4 +35,4 @@ foreach ($item in (Get-Item *.jpg)) {
     }
 }
 Write-Output "Renaming done, fixing dates..."
-& exiftool.exe '-FileModifyDate<DateTimeOriginal' *.dng *.jpg
+& exiftool.exe '-FileModifyDate<DateTimeOriginal' *.dng *.nef *.jpg
