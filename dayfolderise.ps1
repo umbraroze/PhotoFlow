67c5715124693a9208cd8ddee1e1be35677277e9
Rose Midford <wwwwolf@iki.fi>
2023-02-11 21:38:42 +0200

    Added dayfolderise script

---
+++ dayfolderise.ps1
@@ -0,0 +1,16 @@
+# Moves files into day-based subfolders based on modification timestamp.
+
+$infiles = Get-Item *
+$infiles | ForEach-Object {
+    $in = $_;
+    $d = Get-Date -UFormat "%Y-%m-%d" $in.LastWriteTime;
+    $p = Split-Path $in;
+
+    if(-Not (Test-Path $d)) {
+        New-Item -Name $d -ItemType "directory"
+    }
+
+    $out = $p  + "\" + $d + "\" + $in.Name;
+    Write-Output "`n`nProcessing $in => $out`n`n";
+    Move-Item $in $out
+}
